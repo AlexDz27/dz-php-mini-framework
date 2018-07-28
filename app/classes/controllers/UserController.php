1cc59a2c57ccfc08c21af6b0a325c1c5bf46b600
Alexey Dzyuba <alexeydzyuba27@gmail.com>
2018-07-28 21:14:13 +0300

    renamed helpers

--- app/classes/controllers/UserController.php
+++ app/classes/controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\controllers;
 
 use App\models\UserModel;
 
-use App\services\StringHelper as sh;
+use App\helpers\StringHelper as sh;
 
 use Respect\Validation\Validator as v;
 use Respect\Validation\Exceptions\NestedValidationException;
--- app/classes/helpers/StringHelper.php
+++ app/classes/helpers/StringHelper.php
@@ -1,7 +1,7 @@
 <?php
 
 
-namespace App\services;
+namespace App\helpers;
 
 
 class StringHelper
